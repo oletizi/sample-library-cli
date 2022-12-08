24d18d317564b99acecf44189d4ce52c28d822b4
orion <oletizi@mac.com>
2022-12-08 10:42:21 -0800

    - Started adding display views for sample metadata

--- src/view/InfoView.ts
+++ src/view/InfoView.ts
@@ -1,5 +1,6 @@
 import {Node} from "@oletizi/sample-library-manager/dist/Node"
 import {Sample} from "@oletizi/sample-library-manager/dist/Sample"
+import {MediaStreamMeta, SampleMeta} from "@oletizi/sample-library-manager/dist/SampleMeta"
 
 export interface InfoView {
   info(): string
@@ -7,14 +8,54 @@ export interface InfoView {
 
 export class LibrarySampleView implements InfoView {
   private readonly sample: Sample
+
   constructor(sample: Sample) {
     this.sample = sample
   }
+
   toString(): string {
     return this.sample.name
   }
+
   info(): string {
-    return `I'm the sample info for sample: ${this.toString()}`
+    return new SampleMetaInfoView(this.sample.meta).info()
+  }
+}
+
+class SampleMetaInfoView implements InfoView {
+  private readonly sampleMeta: SampleMeta
+
+  constructor(sampleMeta: SampleMeta) {
+    this.sampleMeta = sampleMeta
+  }
+
+  info(): string {
+    return "Keywords: " + Array.from(this.sampleMeta.keywords).join(',')
+      + '\n' + new MediaStreamsInfoView(this.sampleMeta.streams).info()
+  }
+}
+
+class MediaStreamsInfoView implements InfoView {
+  private readonly streams: ReadonlyArray<MediaStreamMeta>
+
+  constructor(streams: ReadonlyArray<MediaStreamMeta>) {
+    this.streams = streams
+  }
+
+  info(): string {
+    return this.streams.map((streamMeta: MediaStreamMeta) => new MediaStreamInfoView(streamMeta).info()).join('\n')
+  }
+}
+
+class MediaStreamInfoView implements InfoView {
+  private streamMeta: MediaStreamMeta
+
+  constructor(streamMeta: MediaStreamMeta) {
+    this.streamMeta = streamMeta
+  }
+
+  info(): string {
+    return `Sample Rate: ${this.streamMeta.sampleRate}`
   }
 }
 
